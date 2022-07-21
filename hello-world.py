659ad86da142e7bd96976a6c7ad4f2e3a5d20713
Shashi Mazumdar <shashi@reali.com>
2022-07-21 12:36:55 -0700

    Changed frequency and added a sleep

--- hello-world.py
+++ hello-world.py
@@ -1,13 +1,15 @@
 from datetime import datetime
+import time
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 
 def print_hello():
+    time.sleep(120)
     return 'Hello world from first Airflow DAG!'
 
 dag = DAG('hello_world', description='Hello World DAG',
-          schedule_interval='0 12 * * *',
+          schedule_interval='@hourly',
           start_date=datetime(2022, 3, 20), catchup=False)
 
 hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)

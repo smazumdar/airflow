197c66a3abb6ddc92e11e1436f00bd54489a48b0
Shashi Mazumdar <shashi@reali.com>
2022-08-04 11:40:21 -0700

    Updated to use cron

--- hello-world.py
+++ hello-world.py
@@ -5,13 +5,12 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 
 def print_hello():
-    for x in range(10):
-        print(f'Hello world from first Airflow DAG! - {x}')
+    for x in range(5):
         time.sleep(120)
     return True
 
 dag = DAG('hello_world', description='Hello World DAG',
-          schedule_interval='@hourly',
+          schedule_interval='45 11,12,13 * * *',
           start_date=datetime(2022, 3, 20), catchup=False)
 
 hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
